8208325fc573067280d0dd5ddb87d3961e4d2521
Tim <yjl5062@hotmail.com>
2020-02-19 22:16:57 +0800

    总体疫情展示完成

---
+++ src/components/category/category.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import styles from './style.css'
+import PropTypes from 'prop-types'
+export class Category extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+    render() {
+        const { title, count, addcount, color } = this.props
+        return (
+            <div style={{ color: color }} className='category'>
+                <p>{title}</p>
+                <p>{count}例</p>
+                <p>
+                    <span className='tip'>较昨日</span>
+                    <span className='add'>+{addcount}</span>
+                </p>
+            </div>
+        )
+    }
+}
+// category.propTypes = {
+//     title: String,
+//     count: Number,
+//     addcount: Number,
+//     color: String
+// }
+export default Category
---
+++ src/pages/Home/home.js
@@ -0,0 +1,159 @@
+import React, { Component } from 'react'
+import { Skeleton } from 'antd'
+import styles from './home.css'
+import dayjs from 'dayjs'
+import { Tabs } from 'antd-mobile'
+import 'antd-mobile/lib/tabs/style/css'
+import { getVirusDataOnTime, getVirusDataStatic, getRumor, getTrend } from '../../services/getData'
+import Category from '../../components/category/category'
+
+export class home extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            timer: null,
+            newsList: [],
+            caseList: [],
+            virusDesc: {
+                confirmedCount: 0,
+                suspectedCount: 0,
+                deadCount: 0,
+                curedCount: 0,
+                seriousCount: 0,
+                modifyTime: 0,
+                note1: '',
+                note2: '',
+                note3: '',
+                remark1: '',
+                remark2: '',
+                confirmedIncr: 0,
+                suspectedIncr: 0,
+                deadIncr: 0,
+                curedIncr: 0,
+                seriousIncr: 0
+            },
+            staticList: [],
+            mapList: [],
+            rumorList: [],
+            provinceName: '', //是否点击了某个省份
+            tabIndex: 0,
+            dateList: [],
+            confirmedTrendList: [],
+            suspectedTrendList: [],
+            deadTrendList: [],
+            curedTrendList: [],
+            provinceList: [],
+            loading: false,
+            trendLoading: true
+        }
+    }
+    componentDidMount() {
+        this.initData()
+        this.setState({
+            timer: setInterval(() => {
+                this.initData()
+            }, 1000 * 60 * 20)
+        })
+    }
+    componentWillUnmount() {
+        const { timer } = this.state
+        clearInterval(timer)
+        this.setState({ timer: null })
+    }
+    initData = async () => {
+        const res = await getVirusDataOnTime()
+        if (res.status === 200) {
+            const { news, desc } = res.data.newslist[0]
+            this.setState({
+                newsList: news,
+                virusDesc: desc,
+                caseList: res.data.newslist[0].case
+            })
+        }
+        const resuslt = await getVirusDataStatic()
+
+    }
+    render() {
+        const {
+            virusDesc,
+            mapList,
+            provinceName,
+            tabIndex,
+            newsList,
+            rumorList,
+            dateList,
+            confirmedTrendList,
+            suspectedTrendList,
+            deadTrendList,
+            curedTrendList,
+            provinceList,
+            loading,
+            trendLoading
+        } = this.state
+        const tabs = [{ title: '疫情地图' }, { title: '最新消息' }, { title: '疫情趋势' }]
+        return (
+            <Skeleton loading={loading} active paragraph={{ rows: 50 }}>
+                <div>
+                    <div className='header-top'>
+                        <p className='header-title'>新型冠状病毒肺炎疫情</p>
+                        <p className='header-tip'>实时动态</p>
+                    </div>
+                    <Tabs
+                        tabs={tabs}
+                        initialPage={0}
+                        swipeable={false}
+                        tabBarInactiveTextColor="#616161"
+                        tabBarActiveTextColor="#6C63FF"
+                        tabBarUnderlineStyle={{ border: '1px #6C63FF solid' }}
+                    >
+
+                        <div className='Map'>
+                            <div className='map-header'>
+                                <div className='map-title'>
+                                    <span>全国</span>
+                                    <p>截至{dayjs(virusDesc.modifyTime).format('YYYY年MM月DD日 HH:mm')}
+                                        (北京时间)</p>
+                                </div>
+                                <div className='map-number'>
+                                    <Category
+                                        title={'确诊'}
+                                        count={virusDesc.confirmedCount}
+                                        addcount={virusDesc.confirmedIncr}
+                                        color={'#e57471'}
+                                    />
+                                    <Category
+                                        title={'疑似'}
+                                        count={virusDesc.suspectedCount}
+                                        addcount={virusDesc.suspectedIncr}
+                                        color={'#dda451'}
+                                    />
+                                    <Category
+                                        title={'重症'}
+                                        count={virusDesc.seriousCount}
+                                        addcount={virusDesc.seriousIncr}
+                                        color={'#5d4037'}
+                                    />
+                                    <Category
+                                        title={'死亡'}
+                                        count={virusDesc.deadCount}
+                                        addcount={virusDesc.deadIncr}
+                                        color={'#919399'}
+                                    />
+                                    <Category
+                                        title={'治愈'}
+                                        count={virusDesc.curedCount}
+                                        addcount={virusDesc.curedIncr}
+                                        color={'#7ebe50'}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </Tabs>
+                </div>
+            </Skeleton>
+
+        )
+    }
+}
+
+export default home
